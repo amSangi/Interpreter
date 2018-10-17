7e408f5c2a95e49645e53f189424be0a0f2a0b78
amSangi <sangiamir95@gmail.com>
2018-10-16 17:26:01 -0700

    Removed tabs

--- include/lexer/types.h
+++ include/lexer/types.h
@@ -2,49 +2,48 @@
 
 enum TokenType {
 	// Literals
-	NumericLiteral,						// [0-9]+ (".")? [0-9]*
+	NumericLiteral,                     // [0-9]+ (".")? [0-9]*
+	// Identifier
+	IdentifierToken,                    // ( "_" | [a-z, A-Z] ) ("_" | [a-z, A-Z] | [0-9])*
 
 	// Punctuation 
-	OpenParanToken,						// (
-	CloseParanToken,					// )
-	OpenBraceToken,						// {
-	CloseBraceToken,					// }
-	CommaToken,							// , 
-	SemiColonToken,						// ;
-	ColonToken,							// :
-	LessThanToken,						// <
-	LessThanEqualToken,					// <=
-	GreaterThanToken,					// >
-	GreaterThanEqualToken,				// >=
-	EqualEqualToken,					// ==
-	EqualToken,							// = 
-	PlusToken,							// +
-	MinusToken,							// -
-	AsteriskToken,						// *
-	ForwardSlashToken,					// /
-	ModToken,							// %
-	DoubleAmpersandToken,				// &&
-	AmpersandToken,						// &
-	ExclamationToken,					// !
-	DoubleBarToken,						// ||
-	BarToken,							// |
-	QuestionToken,						// ?
-
-	// Identifier
-	IdentifierToken,					// ( "_" | [a-z, A-Z] ) ("_" | [a-z, A-Z] | [0-9])*
+	OpenParanToken,                     // (
+	CloseParanToken,                    // )
+	OpenBraceToken,                     // {
+	CloseBraceToken,                    // }
+	CommaToken,                         // ,
+	SemiColonToken,                     // ;
+	ColonToken,                         // :
+	LessThanToken,                      // <
+	LessThanEqualToken,                 // <=
+	GreaterThanToken,                   // >
+	GreaterThanEqualToken,              // >=
+	EqualEqualToken,                    // ==
+	EqualToken,                         // =
+	PlusToken,                          // +
+	MinusToken,                         // -
+	AsteriskToken,                      // *
+	ForwardSlashToken,                  // /
+	ModToken,                           // %
+	DoubleAmpersandToken,               // &&
+	AmpersandToken,                     // &
+	ExclamationToken,                   // !
+	DoubleBarToken,                     // ||
+	BarToken,                           // |
+	QuestionToken,                      // ?
 
 	// Reserved
-	IfKeyword,							// if
-	ElseKeyword,						// else
-	ReturnKeyword,						// return
-	WhileKeyword,						// while
-	ForKeyword,							// for
-	TrueKeyword,						// true
-	FalseKeyword,						// false
-	NumberKeyword,						// num
-	BoolKeyword,						// bool
-	VoidKeyword,						// void
-	MainKeyword,						// main
+	IfKeyword,                          // if
+	ElseKeyword,                        // else
+	ReturnKeyword,                      // return
+	WhileKeyword,                       // while
+	ForKeyword,                         // for
+	TrueKeyword,                        // true
+	FalseKeyword,                       // false
+	NumberKeyword,                      // num
+	BoolKeyword,                        // bool
+	VoidKeyword,                        // void
+	MainKeyword,                        // main
 
 
 	// End
